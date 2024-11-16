69b3fcdbbb4cadf0a57e2a51c729edfe7633d00a
Noah Huppert <contact@noahh.io>
2024-11-16 17:51:47 -0500

    init nextjs and started header

---
+++ ekicat/src/app/layout.tsx
@@ -0,0 +1,56 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import Link from "next/link";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "eki.cat",
+  description: "Trains",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <header className="bg-background fg-foreground flex flex-row px-4">
+          <nav className="flex flex-row grow h-14">
+            <Link
+              href="/"
+              className="grow self-center"
+            >
+              eki.cat
+            </Link>
+
+            <Link
+              href="/photos"
+              className="self-center"
+            >
+              Photos
+            </Link>
+          </nav>
+        </header>
+
+        <div className="p-4">
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
---
+++ ekicat/src/app/photos/page.tsx
@@ -0,0 +1,7 @@
+export default function Photos() {
+    return (
+        <div>
+            Here is a list of photos!
+        </div>
+    )
+}
