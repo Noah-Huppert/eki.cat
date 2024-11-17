1fd3d042a78c1defda885b514e46b6e090d2c140
Noah Huppert <contact@noahh.io>
2024-11-16 22:47:09 -0500

    rm header from home page

--- ekicat/src/app/page.tsx
+++ ekicat/src/app/page.tsx
@@ -2,22 +2,8 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <header className="bg-background fg-foreground flex flex-row px-4">
-      <nav className="flex flex-row grow h-14">
-        <Link
-          href="/"
-          className="grow self-center"
-        >
-          eki.cat
-        </Link>
-
-        <Link
-          href="/photos"
-          className="self-center"
-        >
-          Photos
-        </Link>
-      </nav>
-    </header>
+    <div>
+      Home
+    </div>
   );
 }
