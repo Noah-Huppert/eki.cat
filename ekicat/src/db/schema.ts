e3d53066561067bf6def8bb39113f3cde6982683
Noah Huppert <contact@noahh.io>
2024-11-18 23:21:40 -0500

    defined relations and types

--- ekicat/src/db/schema.ts
+++ ekicat/src/db/schema.ts
@@ -27,6 +27,7 @@ export const mediaBlob = pgTable("media_blobs", {
     mimeType: text().notNull(),
     data: bytea().notNull(),
 });
+export type MediaBlobRow = typeof mediaBlob.$inferInsert;
 
 /**
  * Any type of location.
@@ -37,38 +38,100 @@ export const location = pgTable("locations", {
     id: integer().primaryKey(),
     geometry: geometry().notNull(),
 });
+export type LocationRow = typeof location.$inferInsert;
 
 /**
  * Node in graph of information represented by website.
+ * @param id - Unique identifier
  **/
 export const node = pgTable("nodes", {
     id: integer().primaryKey(),
-    pathId: text().notNull(),
-    path: ltree().notNull(),
-    locationId: integer().notNull().references(() => location.id),
 });
+export type NodeRow = typeof node.$inferInsert;
 
+/**
+ * Relations for a {@link node}
+ **/
+export const nodeRelations = relations(node, ({ one, many }) => ({
+    locations: many(nodeLocation),
+    edges: many(nodeEdge),
+    mediaBlobs: many(nodeMediaBlob),
+}));
+
+/**
+ * Places a node in an undirected graph.
+ * @param id - Unique identifier
+ * @param sourceNodeId - The node of which an edge is being defined
+ * @param targetNodeId - The node between which an edge exists
+ **/
 export const nodeEdge = pgTable("node_edges", {
     id: integer().primaryKey(),
-    originNodeId: integer().notNull(),
-    targetNodeId: integer().notNull(),
+    sourceNodeId: integer().notNull().references(() => node.id),
+    targetNodeId: integer().notNull().references(() => node.id),
 });
+export type NodeEdgeRow = typeof nodeEdge.$inferInsert;
 
-export const nodeRelations = relations(node, ({ one, many }) => ({
-    location: one(location),
-    nodeMediaBlobs: many(nodeMediaBlob),
-})) 
+/**
+ * Relations for {@link nodeEdge}.
+ */
+export const nodeEdgeRelations = relations(nodeEdge, ({ one }) => ({
+    sourceNode: one(node, {
+        fields: [nodeEdge.sourceNodeId],
+        references: [node.id],
+    }),
+    targetNode: one(node, {
+        fields: [nodeEdge.targetNodeId],
+        references: [node.id],
+    }),
+}));
+
+/**
+ * Links a location to a node.
+ * @param id - Unique identifier
+ * @param nodeId - Node being linked
+ * @param locationId - Location being linked
+ **/
+export const nodeLocation = pgTable("node_locations", {
+    id: integer().primaryKey(),
+    nodeId: integer().notNull(),
+    locationId: integer().notNull(),
+});
+export type NodeLocationRow = typeof nodeLocation.$inferInsert;
+
+export const nodeLocationRelations = relations(nodeLocation, ({ one }) => ({
+    node: one(node, {
+        fields: [nodeLocation.nodeId],
+        references: [node.id],
+    }),
+    location: one(node, {
+        fields: [nodeLocation.locationId],
+        references: [location.id],
+    }),
+}));
 
 /**
  * Links media blobs to nodes.
+ * @param id - Unique identifier
+ * @param nodeId - The node to which the media blob is being linked
+ * @param mediaBlobId - ID of media blob being linked
  **/
 export const nodeMediaBlob = pgTable("node_media_blobs", {
     id: integer().primaryKey(),
     nodeId: integer().notNull().references(() => node.id),
     mediaBlobId: integer().notNull().references(() => mediaBlob.id),
 });
+export type NodeMediaBlobRow = typeof nodeMediaBlob.$inferInsert;
 
+/**
+ * Relations for {@link nodeMediaBlob}
+ **/
 export const nodeMediaBlobRelations = relations(nodeMediaBlob, ({ one }) => ({
-    node: one(node),
-    mediaBlob: one(mediaBlob),
+    node: one(node, {
+        fields: [nodeMediaBlob.nodeId],
+        references: [node.id],
+    }),
+    mediaBlob: one(mediaBlob, {
+        fields: [nodeMediaBlob.mediaBlobId],
+        references: [mediaBlob.id],
+    }),
 }));
